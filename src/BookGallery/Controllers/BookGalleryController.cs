80646ef1b9cdd931977698895bd091df6edc970e
Marcus Wallen <marcus.wallen@knowit.se>
2017-10-14 22:44:09 +0200

    Adderade Controller

---
+++ src/BookGallery/Controllers/BookGalleryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookGallery.Controllers
+{
+    public class BookGalleryController : Controller
+    {
+
+
+        public ActionResult Detail()
+        {
+            return new RedirectResult("/", true);
+        }
+        // GET: BookGallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/BookGallery/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ContentResult Index()
+        {
+            return new ContentResult()
+            {
+                Content ="hej igen fårn content result"
+            };
+        }
+    }
+}
